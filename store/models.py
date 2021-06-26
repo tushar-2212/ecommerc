83434861f315bd929f87f9cec054efa3643080ea
tushar-2212 <72065122+tushar-2212@users.noreply.github.com>
2021-06-26 21:07:43 +0530

    first git commmit

--- store/models.py
+++ store/models.py
@@ -15,9 +15,19 @@ class Product(models.Model):
     name = models.CharField(max_length=200, null=True)
     price = models.FloatField()
     digitals = models.BooleanField(default=False,null=True, blank=False)
+    image = models.ImageField(null=True, blank=True)
     
     def __str__(self):
         return self.name
+
+    @property
+    def imageURL(self):
+        try:
+            url = self.imageURL
+        except:
+            url = ''
+        return url            
+
 
 class Order(models.Model):
     customer = models.ForeignKey(Customer, on_delete=models.SET_NULL,blank=True, null=True)
@@ -28,12 +38,44 @@ class Order(models.Model):
     def __str__(self):
         return str(self.id)
 
+    @property
+    def get_cart_total(self):
+        orderitems = self.orderitem_set.all()
+
+        total =sum([item.get_total for item in orderitems])
+
+        return total
+    @property
+    def get_cart_items(self):
+        orderitems = self.orderitem_set.all()
+        total = sum([item.quantity for item in orderitems])
+        return total       
+
+
 class OrderItem(models.Model):
     product = models.ForeignKey(Product, on_delete=models.SET_NULL,blank=True, null=True)
     order = models.ForeignKey(Order, on_delete=models.SET_NULL,blank=True, null=True)
     quantity = models.IntegerField(default=0, null=True, blank=True)
     date_added = models.DateTimeField(auto_now_add=True)
-    
+
+    @property
+    def get_total(self):
+        total = self.product.price * self.quantity
+        return total
+
+
+class ShippingAddress(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.SET_NULL,blank=True,null=True)
+    order = models.ForeignKey(Order, on_delete=models.SET_NULL,blank=True, null=True)
+    address = models.CharField(max_length=200, null=True)
+    city = models.CharField(max_length=200, null=True)
+    state = models.CharField(max_length=200, null=True)
+    zipcode = models.CharField(max_length=200, null=True)
+    date_added = models.CharField(max_length=200, null=True)
+ 
+    def __str__(self):
+       return self.address
+
 
 
 
--- store/views.py
+++ store/views.py
@@ -1,13 +1,30 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from .models import *
 def store(request):
-    context={}
+    products = Product.objects.all
+    context={'products' :products}
     return render(request, 'store/store.html', context)
 
 def cart(request):
-    context={}
+
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order ,created = Order.objects.get_or_create (customer=customer,complete=False)
+        items = order.orderitem_set.all()
+    else:
+        items =[]
+        order = {'get_cart_total':0, 'get_cart_item':0, 'shipping': False}  
+        cartItems = order['get_cart_items']
+
+    context={'items' :items , 'order' :order}
     return render(request, 'store/cart.html', context)
 
 def checkout(request):
     context={}
     return render(request, 'store/checkout.html', context)    
 # Create your views here.
+
+def updateItems(request):
+    return JsonResponse('Item was added',safe=False)
+
